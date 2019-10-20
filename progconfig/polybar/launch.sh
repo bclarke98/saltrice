1bba58f0cd64e732ba1bce2e79ace096f1f7e11a
bclarke98 <bclarke98@outlook.com>
2019-10-20 13:28:04 -0400

    added ability to hide gaps and polybar with mod+shift+g, restored with mod+g

--- progconfig/polybar/launch.sh
+++ progconfig/polybar/launch.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
-# polybar d3xbar&
-polybar example&
+# ./launch.sh 0 : kills ('hides') polybar, ./launch.sh or ./launch.sh 1 restarts it
+killall -q polybar; [[ -z $1 || $1 -eq 1 ]] && while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done && polybar example&
